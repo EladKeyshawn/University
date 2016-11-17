33581d2c0669b694a264521f0de13902e17a2771
Elad Keyshawn <elad.keyshawn@gmail.com>
2016-11-17 16:17:30 +0200

    clean up ex2_1

--- C_course/ex2/MyString.c
+++ C_course/ex2/MyString.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 
 int extendedSubStr(int isCyclic, int step, const char* str1, const char* str2) {
     int len1 = 0;
@@ -7,8 +6,7 @@ int extendedSubStr(int isCyclic, int step, const char* str1, const char* str2) {
         len1++;
         r1++;
     }
-    
-    
+
     if (*str1 == '\0' || *str2 == '\0') {
         return -1;
     }
@@ -22,17 +20,13 @@ int extendedSubStr(int isCyclic, int step, const char* str1, const char* str2) {
             temp=i+1;
             while(str1[i]==str2[j] && str2[j] != '\0')
             {
-                 if(str1[i+1] == '\0' && isCyclic == 1) {
-                   i = step-1; // end of string cycle back
-                   j++; // str2 keep going
-                 } else {
-                     if (i + step > len1-1 ) {
-                         i = step-(len1-i); // iter on str1
-                     } else {
-                         i += step; // iter on str1
-                     }
-                   j++; // iter on str2
-                 }
+                if (i + step > len1-1 && isCyclic == 1 ) {
+                    i = step-(len1-i); // iter on str1
+                } else {
+                    i += step; // iter on str1
+                }
+                
+                j++;
                 
             }
             if(str2[j] == '\0') {
@@ -44,7 +38,5 @@ int extendedSubStr(int isCyclic, int step, const char* str1, const char* str2) {
         }
     }
 
-
     return -1;
-
 }
--- C_course/ex2/main.c
+++ C_course/ex2/main.c
@@ -6,9 +6,9 @@
 int main(int argc, char* argv[]) {
 
     const char* str1 = "keyshawnf";
-    const char* str2 = "eak";
+    const char* str2 = "keyshawn";
 
-    printf("index: %d\n",extendedSubStr(1, 4, str1, str2));
+    printf("index: %d\n",extendedSubStr(1, 1, str1, str2));
 
     return 0;
 }
